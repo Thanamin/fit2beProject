392389720a7688ea81df7ae51fee8a69bb0a4b5b
Thanamin Akkharananwinit <mr.kan0m@hotmail.com>
2022-08-25 01:02:10 +0700

    update api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
   //  baseURL: 'http://localhost:8080',
- baseURL: 'https://fit2b-backend.vercel.app/',
+ baseURL: 'https://fit2be-project-backend-thanamin.vercel.app/',
   //withCredentials: true, 
   // เพื่อให้ส่ง cookie ได้
 });
